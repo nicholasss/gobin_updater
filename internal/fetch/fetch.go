60a89865886cec8ebd1dcd922dff53ab32363692
Nicholas <nicholasss@users.noreply.github.com>
2025-05-15 16:02:48 -0400

    adding caching functionality

--- internal/fetch/fetch.go
+++ internal/fetch/fetch.go
@@ -3,10 +3,14 @@ package fetch
 import (
 	"encoding/json"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 const (
 	GoVersionListURL = "https://go.dev/dl/?mode=json&include=all"
+	CacheDirName     = "goblin"
+	CacheFileName    = "goblin_versions.json"
 )
 
 type GoVersionList struct {
@@ -14,6 +18,41 @@ type GoVersionList struct {
 	Stable  bool   `json:"stable"`
 }
 
+func checkForCache()
+
+func loadFromCache()
+
+func saveToCache(versionList *[]GoVersionList) error {
+	// get cache path
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return err
+	}
+
+	toolCacheDir := filepath.Join(cacheDir, CacheDirName)
+	err = os.MkdirAll(toolCacheDir, 0755)
+	if err != nil {
+		return err
+	}
+
+	// create dir if needed
+	toolCachePath := filepath.Join(toolCacheDir, CacheFileName)
+	toolCacheFile, err := os.Create(toolCachePath)
+	if err != nil {
+		return err
+	}
+	defer toolCacheFile.Close()
+
+	// save to json
+	err = json.NewEncoder(toolCacheFile).Encode(versionList)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// fetches version list
 func FetchGoVersionList() (*[]GoVersionList, error) {
 	resp, err := http.Get(GoVersionListURL)
 	if err != nil {
